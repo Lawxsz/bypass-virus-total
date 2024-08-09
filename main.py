e94c578c1156fc973f0e2a99a9da489efb8c2f8e
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2024-08-09 07:50:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import uuid
 import re
 import requests
 
-blacklisted_ips = ['192.168.1.100', '203.0.113.5']
-blacklisted_macs = ['00:1A:2B:3C:4D:5E', '00:1B:44:11:3A:B7']
-blacklisted_hwids = ['1234567890', '0987654321']
+blacklisted_ips = ['']
+blacklisted_macs = ['']
+blacklisted_hwids = ['']
 blacklisted_gpus = ['VirtualBox Graphics Adapter', 'VMware SVGA II']
 
 def get_system_info():
